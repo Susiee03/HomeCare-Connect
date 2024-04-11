ca6167c577eee04077c8b7dce7fbc1c569fa32a9
jeffrey615-creator <131724971+jeffrey615-creator@users.noreply.github.com>
2024-04-10 19:22:08 -0700

    upload photo to firebase but need to figure the photo display

--- Firebase/FirebaseSetup.js
+++ Firebase/FirebaseSetup.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import {initializeAuth, getReactNativePersistence} from "firebase/auth";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import {apiKey, authDomain, projectId, storageBucket, messagingSenderId , appId} from "@env";
@@ -20,7 +21,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app)
-
+export const storage = getStorage(app);
 
 export const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
